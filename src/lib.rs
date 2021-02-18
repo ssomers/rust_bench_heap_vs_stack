8ba1f010a4e7be0241443a8cb4786c56b90b2ed5
Stein Somers <git@steinsomers.be>
2021-02-18 06:50:54 +0100

    set parent_id to non-zero & cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -23,6 +23,15 @@ impl Node {
         }
     }
 
+    fn init(this: *mut Self) {
+        unsafe {
+            addr_of_mut!((*this).parent).write(None);
+            //addr_of_mut!((*this).parent_idx).write(MaybeUninit::uninit());
+            //addr_of_mut!((*this).parent_idx).write(MaybeUninit::new(0));
+            addr_of_mut!((*this).len).write(0);
+        }
+    }
+
     fn stack_new() -> Box<Self> {
         Box::new(Node {
             parent: None,
@@ -30,38 +39,26 @@ impl Node {
             len: 0,
         })
     }
+}
 
-    fn init(this: *mut Self) {
-        unsafe {
-            addr_of_mut!((*this).parent).write(None);
-            addr_of_mut!((*this).parent_idx).write(MaybeUninit::new(0));
-            addr_of_mut!((*this).len).write(0);
-        }
+fn grey_box(node1: Box<Node>, node2: Box<Node>) {
+    unsafe {
+        let node1 = NonNull::from(Box::leak(node1));
+        let mut node2 = NonNull::from(Box::leak(node2));
+        node2.as_mut().parent = Some(node1);
+        node2.as_mut().parent_idx.write(42);
+        test::black_box(node2);
+        Global.deallocate(node2.cast(), Layout::new::<Node>());
+        Global.deallocate(node1.cast(), Layout::new::<Node>());
     }
 }
 
 #[bench]
 fn heap(b: &mut test::Bencher) {
-    b.iter(|| unsafe {
-        let node1 = NonNull::from(Box::leak(Node::heap_new()));
-        let mut node2 = NonNull::from(Box::leak(Node::heap_new()));
-        node2.as_mut().parent = Some(node1);
-        node2.as_mut().parent_idx.write(0);
-        test::black_box(node2);
-        Global.deallocate(node2.cast(), Layout::new::<Node>());
-        Global.deallocate(node1.cast(), Layout::new::<Node>());
-    })
+    b.iter(|| grey_box(Node::heap_new(), Node::heap_new()))
 }
 
 #[bench]
 fn stack(b: &mut test::Bencher) {
-    b.iter(|| unsafe {
-        let node1 = NonNull::from(Box::leak(Node::stack_new()));
-        let mut node2 = NonNull::from(Box::leak(Node::stack_new()));
-        node2.as_mut().parent = Some(node1);
-        node2.as_mut().parent_idx.write(0);
-        test::black_box(node2);
-        Global.deallocate(node2.cast(), Layout::new::<Node>());
-        Global.deallocate(node1.cast(), Layout::new::<Node>());
-    })
+    b.iter(|| grey_box(Node::stack_new(), Node::stack_new()))
 }
